05a6790ea1f27e0e9b27c381b28e181c121ab81f
Maciej Rainko <maciek.rainko@gmail.com>
2019-04-01 23:40:41 +0200

    Second_homework

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,51 @@ var path = require("path");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/js/index.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "")
+    path: path.resolve(__dirname, "dist")
   },
-  plugins: [new HtmlWebpackPlugin()]
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "postcss-loader"]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "postcss-loader" },
+          { loader: "sass-loader" }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "images/",
+              publicPath: "images/"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      title: "Homework_2",
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
 };
